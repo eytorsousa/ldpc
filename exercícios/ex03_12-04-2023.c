6008ddcd449472f1e798fdb015f306ac4b928da0
Eytor Lima <eytorlima@outlook.com>
2023-04-12 21:54:36 -0300

    Add files via upload

---
+++ exercícios/ex03_12-04-2023.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+//Analisando dois valores e verificando sua divisão inteira e seu resto.
+
+int main(){
+
+int n1 = 0;
+int n2 = 0;
+
+printf("\tOlá, vamos analisar dois valores!\n");
+printf("Insira o primeiro número: ");
+scanf("%d", &n1);
+printf("Insira o segundo número: ");
+scanf("%d", &n2);
+
+printf("\n\tDIVISÕES INTEIRAS\n");
+printf("%d/%d = %d", n1, n2, n1/n2);
+printf("\n%d/%d = %d", n2, n1, n2/n1);
+
+printf("\n\tRESTO DA DIVISÃO\n");
+printf("%d/%d = %d", n1, n2, n1%n2);
+printf("\n%d/%d = %d", n2, n1, n2%n1);
+
+printf("\n\n\tFIM DO PROGRAMA");
+return 0;
+}
