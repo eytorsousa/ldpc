d4324079934b5cce770de6bbe52875d945045ba4
Eytor Lima <eytorlima@outlook.com>
2023-04-26 21:35:22 -0300

    Add files via upload

---
+++ tarefas/26-04-2023 aula.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Introduzindo o comando while */
+//Exemplo
+
+int main()
+{
+    int cont = 1;
+    while(cont <= 10){
+        printf("%d ", cont);
+        cont += 1;
+    }
+
+    return 0;
+}
