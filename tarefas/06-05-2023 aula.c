6261b9b01103de88dc2f065a445349999fe90bf2
Eytor Lima <eytorlima@outlook.com>
2023-05-06 10:52:01 -0300

    Add files via upload

---
+++ tarefas/06-05-2023 aula.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Aula 06-05-2023
+// Utilizando for - estrutura de repetição.
+int main(){
+    int k = 0;
+    printf("Exibindo valores de 1 a 10:\n");
+    for(k = 0; k <= 10; k++){
+        printf("%d ", k);
+    }
+    return 0;
+}
